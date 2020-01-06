26e701f5c0a5481cece039614c4068b63b39d04d
fightwjl <1395182110@qq.com>
2020-01-06 10:52:36 +0800

    hello3

--- hello.java
+++ hello.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello Word");
 	System.out.println("1");
-	System.out.println("2");
 	System.out.println("Hello Word");
+	System.out.println("2");
 	
     }
 }
